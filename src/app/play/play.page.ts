a85e2fdd68f08d216f205b2d30e998737e1537eb
Bob LB4 Liu <liulb4@lenovo.com>
2019-02-12 13:53:13 +0800

    Numbers can clicked.

--- src/app/play/play.page.ts
+++ src/app/play/play.page.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayPage implements OnInit {
 
-  constructor() {}
+  constructor() { }
+
+  numArr: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  soduArr: Array<number>
+  backupSoduArr: Array<number>
+  blankArr: Array<number>
+  soduEditArr: Array<number>
+  soduShow = {
+    editNumber: null
+  }
 
   ngOnInit() {
     this.createSoduArr()
@@ -17,15 +26,8 @@ export class PlayPage implements OnInit {
     console.log('backupSoduArr: ' + this.backupSoduArr)
     console.log('numArr: ' + this.numArr)
     console.log('blankArr: ' + this.blankArr)
+    console.log('soduEditArr: ' + this.soduEditArr)
   }
-
-
-
-  numArr: Array < number > = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  soduArr: Array < number > ;
-  backupSoduArr: Array < number > ;
-  blankArr: Array < number > ;
-  soduEditArr: Array < number > ;
 
   // 生成数独
   createSoduArr() {
@@ -37,26 +39,26 @@ export class PlayPage implements OnInit {
       // console.log(soduArr)
       for (let i = 1; i <= 9; i++) {
         for (let j = 1; j <= 9; j++) {
-          if (this.soduArr[parseInt(i + '' + j)]) {
+          if (this.soduArr[i * 10 + j]) {
             continue
           }
-          let XArr = this.getXArr(i, this.soduArr)
-          let YArr = this.getYArr(j, this.soduArr)
-          let thArr = this.getThArr(i, j, this.soduArr)
-          let arr = this.getConnect(this.getConnect(XArr, YArr), thArr)
-          let ableArr = this.arrMinus(this.numArr, arr)
+          const XArr = this.getXArr(i, this.soduArr)
+          const YArr = this.getYArr(j, this.soduArr)
+          const thArr = this.getThArr(i, j, this.soduArr)
+          const arr = this.getConnect(this.getConnect(XArr, YArr), thArr)
+          const ableArr = this.arrMinus(this.numArr, arr)
           if (ableArr.length === 0) {
             this.createSoduArr()
             return
           }
 
           let item: number
-          // 如果生成的重复了就重新生成。看起来这里只是以防万一的做法。
+          // 如果生成的重复了就重新生成，这里只是以防万一的做法。
           do {
             item = ableArr[this.getRandom(ableArr.length) - 1]
           } while ((arr.indexOf(item) > -1))
 
-          this.soduArr[parseInt(i + '' + j)] = item
+          this.soduArr[i * 10 + j] = item
         }
       }
       this.backupSoduArr = this.soduArr.slice()
@@ -69,6 +71,7 @@ export class PlayPage implements OnInit {
   reCreate() {
     this.createSoduArr()
   }
+
   newGame() {
     this.createSoduArr()
     this.createBlank(10)
@@ -77,15 +80,19 @@ export class PlayPage implements OnInit {
 
   createBlank(num: number) {
     // 生成指定数量的空白格子的坐标。
-    let arr = []
-    let item
+    const arr = []
+    let item: number
     for (let a = 0; a < num; a++) {
       do {
-        item = parseInt(this.numArr[this.getRandom(9) - 1] + '' + this.numArr[this.getRandom(9) - 1])
+        item = this.numArr[this.getRandom(9) - 1] * 10 + this.numArr[this.getRandom(9) - 1]
       } while (arr.indexOf(item) > -1)
       arr.push(item)
     }
     this.blankArr = arr
+  }
+
+  seeIfBlank(num: number) {
+    return this.blankArr.indexOf(num) === -1
   }
 
   createSoduEdit() {
@@ -102,22 +109,27 @@ export class PlayPage implements OnInit {
       }
     }
   }
-  seeIfBlank(num: number) {
-    return this.blankArr.indexOf(num) == -1
+
+  setShowEditNumber(num: number): void {
+    this.soduShow.editNumber = num
+  }
+
+  setEditArrNumber(num: number): void {
+    this.soduEditArr[this.soduShow.editNumber] = num
   }
 
   creatThird(i: number, j: number) {
     // 为对角线上的三个三宫格随机生成。
-    let sortedNumArr = this.numArr.slice().sort(function () {
+    const sortedNumArr = this.numArr.slice().sort(function () {
       return Math.random() > 0.5 ? -1 : 1
     })
-    let centerNum = parseInt(i + '' + j)
+    const centerNum = i * 10 + j
     /***********
      * 11 12 13
      * 21 22 23
      * 31 32 33
      ***********/
-    let thIndexArr = [
+    const thIndexArr = [
       centerNum - 11, centerNum - 10, centerNum - 9,
       centerNum - 1, centerNum, centerNum + 1,
       centerNum + 9, centerNum + 10, centerNum + 11
@@ -134,11 +146,11 @@ export class PlayPage implements OnInit {
    * @param {Array} dataArr dataArr
    * @returns
    */
-  getYArr(j: number, dataArr: Array < number > ) {
-    let arr = []
+  getYArr(j: number, dataArr: Array<number>) {
+    const arr = []
     for (let a = 1; a <= 9; a++) {
-      if (dataArr[parseInt(a + '' + j)]) {
-        arr.push(dataArr[parseInt(a + '' + j)])
+      if (dataArr[a * 10 + j]) {
+        arr.push(dataArr[a * 10 + j])
       }
     }
     return arr
@@ -151,11 +163,11 @@ export class PlayPage implements OnInit {
    * @param {Array} dataArr dataArr
    * @returns
    */
-  getXArr(i: number, dataArr: Array < number > ) {
-    let arr = []
+  getXArr(i: number, dataArr: Array<number>) {
+    const arr = []
     for (let a = 1; a <= 9; a++) {
-      if (dataArr[parseInt(i + '' + a)]) {
-        arr.push(dataArr[parseInt(i + '' + a)])
+      if (dataArr[i * 10 + a]) {
+        arr.push(dataArr[i * 10 + a])
       }
     }
     return arr
@@ -169,10 +181,10 @@ export class PlayPage implements OnInit {
    * @param {*} dataArr dataArr
    * @returns
    */
-  getThArr(i: number, j: number, dataArr: Array < number > ) {
-    let arr = []
-    let centerNum = this.getTh(i, j)
-    let thIndexArr = [
+  getThArr(i: number, j: number, dataArr: Array<number>) {
+    const arr = []
+    const centerNum = this.getTh(i, j)
+    const thIndexArr = [
       centerNum - 11, centerNum - 10, centerNum - 9,
       centerNum - 1, centerNum, centerNum + 1,
       centerNum + 9, centerNum + 10, centerNum + 11
@@ -186,15 +198,15 @@ export class PlayPage implements OnInit {
   }
   // 获取所在三宫格的中间位坐标。
   getTh(i: number, j: number) {
-    let cenArr = [22, 25, 28, 52, 55, 58, 82, 85, 88]
+    const cenArr = [22, 25, 28, 52, 55, 58, 82, 85, 88]
     // i为0 1 2 j逢3进1 0-8
-    let index = (Math.ceil(i / 3) - 1) * 3 + Math.ceil(j / 3) - 1
-    let centerNum = cenArr[index]
+    const index = (Math.ceil(i / 3) - 1) * 3 + Math.ceil(j / 3) - 1
+    const centerNum = cenArr[index]
     return centerNum
   }
   // 两个简单数组的并集
-  getConnect(arr1: Array < number > , arr2: Array < number > ) {
-    let resArr = arr2.slice()
+  getConnect(arr1: Array<number>, arr2: Array<number>) {
+    const resArr = arr2.slice()
     for (let i = 0; i < arr1.length; i++) {
       if (arr2.indexOf(arr1[i]) < 0) {
         resArr.push(arr1[i])
@@ -203,8 +215,8 @@ export class PlayPage implements OnInit {
     return resArr
   }
   // 两个简单数组差集，arr1为大数组
-  arrMinus(arr1: Array < number > , arr2: Array < number > ) {
-    let resArr: Array < number > = []
+  arrMinus(arr1: Array<number>, arr2: Array<number>) {
+    const resArr: Array<number> = []
     for (let i = 0; i < arr1.length; i++) {
       if (arr2.indexOf(arr1[i]) < 0) {
         resArr.push(arr1[i])
@@ -213,7 +225,7 @@ export class PlayPage implements OnInit {
     return resArr
   }
   // 生成随机正整数
-  getRandom(n: number) {
+  getRandom(n: number): number {
     return Math.floor(Math.random() * n + 1)
   }
 }
